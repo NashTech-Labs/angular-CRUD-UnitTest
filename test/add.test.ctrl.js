63c4f9685226980955b8673cf322cfc50f0edb96
sumitknoldus <sumit@knoldus.com>
2016-04-20 12:40:07 +0530

    refactoring

--- test/add.test.ctrl.js
+++ test/add.test.ctrl.js
@@ -2,8 +2,8 @@ describe('AddCtrl', function() {
 
     beforeEach(module('rbApp'));
     beforeEach(module('ui.router'));
-    var mockScope,mockrootScope,mockState, mockAddFactory, mockLog,mockAddCtrl;
-    var $controller;
+    var mockScope,mockrootScope,mockState, mockAddFactory, mockLog,mockAddCtrl,mockController;
+
 
     beforeEach(inject(function($injector,_$controller_, _$rootScope_, _$state_,_addFactory_, _$log_){
         mockrootScope = _$rootScope_;
@@ -11,43 +11,34 @@ describe('AddCtrl', function() {
         mockState = _$state_;
         mockAddFactory = _addFactory_;
         mockLog = _$log_;
-        // The injector unwraps the underscores (_) from around the parameter names when matching
-        $controller = _$controller_;
-        mockAddCtrl = $controller('AddCtrl', {'$scope': mockScope, '$state': mockState, 'addFactory': mockAddFactory, '$log' : mockLog});
+        mockController = _$controller_;
+        mockAddCtrl = mockController('AddCtrl', {'$scope': mockScope, '$state': mockState, 'addFactory': mockAddFactory, '$log' : mockLog});
     }));
 
-    //'$scope', '$state', 'addFactory', '$log'
+    it("Test: mockAddCtrl exists or not", function() {
+
+        expect(mockAddCtrl).toBeDefined();
+    });
+
     describe('$scope.addEmployee', function() {
+
         it('should run Successfully', function() {
             spyOn(mockState, 'go');
             mockAddFactory.addFactoryMethod = jasmine.createSpy("addFactoryMethod").and.callFake(function(){
-                function Res(){
+                function Response(){
 
                 }
-                Res.prototype.success =function(func){
+                Response.prototype.success =function(func){
                     func();
-                    return new Res();
+                    return new Response();
                 };
-                Res.prototype.error =function(func){
+                Response.prototype.error =function(func){
                 };
-                return new Res();
+                return new Response();
             });
             mockScope.addEmployee();
             expect(mockState.go).toHaveBeenCalledWith('home');
 
-        });
-
-        //
-        //it('sets the strength to "weak" if the password length <3 chars', function() {
-        //    var $scope = {};
-        //    var controller = $controller('PasswordController', { $scope: $scope });
-        //    $scope.password = 'a';
-        //    $scope.grade();
-        //    expect($scope.strength).toEqual('weak');
-        //});
-        it("Test: mockAddCtrl exists or not", function() {
-
-            expect(mockAddCtrl).toBeDefined();
         });
 
     });
